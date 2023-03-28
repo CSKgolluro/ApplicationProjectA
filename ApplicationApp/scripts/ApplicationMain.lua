e1d578305578b314b90aba18617bb9c56770377a
Roy Gollub <roy.gollub@sick.de>
2023-03-28 14:43:36 +0200

    * Added Subtree of AppA v0.1.0

---
+++ ApplicationApp/scripts/ApplicationMain.lua
@@ -0,0 +1,5 @@
+local function main()
+  print("Hi Application, ver 0.1.0")
+end
+Script.register("Engine.OnStarted", main)
+-- serve API in global scope
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ApplicationProjectA
+Added subtree
